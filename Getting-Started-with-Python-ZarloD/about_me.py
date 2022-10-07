2669063af07a41e7294c2c5e17f2a2ea09e22a4e
ZarloD <zarlo415@gmail.com>
2022-10-06 18:39:03 -0700

    completing first python activity files

---
+++ Getting-Started-with-Python-ZarloD/about_me.py
@@ -0,0 +1,8 @@
+name = 'Zarlo Diaz'
+nickname = name
+age = 23
+python_exp = True
+hobbies = 'Gaming, music, food'
+dictionary = {'Movie': 'Whiplash', 'Food': 'Aji de Gallina', 'Hobby': 'Gaming'}
+
+all_vars = dict(vars())
